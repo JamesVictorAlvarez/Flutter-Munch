1d79eb8a771438a18186dc11bcfd47eadf354a77
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2023-05-03 21:00:04 -0400

    Added sign-in, register with firebase auth

--- Munch/lib/main.dart
+++ Munch/lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+// import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'widget_tree.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primaryColor: Color(0xFFE85852),
+//       ),
+//       home: App(),
+//     );
+//   }
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      debugShowCheckedModeBanner: false,
+        theme: ThemeData(
         primaryColor: Color(0xFFE85852),
       ),
-      home: App(),
+      home: WidgetTree(),
     );
   }
 }
